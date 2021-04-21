d6fec723228bff8b6d2456728b9f6bf137f7dd98
Turtle <hypoturtle+gh@gmail.com>
2021-04-22 00:10:54 +0100

    Allow functional

--- GameCanvas.gd
+++ GameCanvas.gd
@@ -1,16 +1,14 @@
 extends CanvasLayer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Menu.hide()
+	$Exit.hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Allow_pressed():
+	if $River.get_child_count():
+		var ducky = $River.get_child(0)
+		$River.remove_child(ducky)
+		$Pond.add_child(ducky)
